e2610d4bd9eb81c5825e8dc3a994d1ce7617831b
Andre Treib <andre.schumacher.treib@gmail.com>
2025-02-07 11:49:52 -0300

    feat: update favicon and logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,14 +4,25 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { ProjectsSheet } from "@/components/projects-sheet";
 import { SettingsSheet } from "@/components/settings-sheet";
 import Link from "next/link";
+import Image from "next/image";
 import { DocumentationSidebar } from "./documentation-sidebar";
+import { useTheme } from "next-themes";
+import { cn } from "@/lib/utils";
 
 export function Navbar() {
+  const { theme } = useTheme();
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="w-full flex h-14 items-center justify-center px-12">
         <Link href="/" className="font-semibold">
-          Cloudret
+          <Image
+            src="/logo.png"
+            alt="Cloudret"
+            width={120}
+            height={56}
+            className={cn(theme === "dark" && "invert")}
+          />
         </Link>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <nav className="flex items-center space-x-2">
