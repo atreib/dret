7cff6533f0b9f933cbe9e9176ec3f25e5de30c78
Andre Treib <andre.schumacher.treib@gmail.com>
2025-02-10 18:36:52 -0300

    fix: footer mobile layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-t">
-      <div className="flex h-14 items-center justify-center">
+      <div className="flex flex-col lg:flex-row py-4 lg:py-0 lg:h-14 items-center justify-center">
         <p className="text-sm text-muted-foreground">
           Built with Next.js, Shadcn/ui, and React Flow
         </p>
